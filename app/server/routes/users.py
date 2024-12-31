3da8bf1055cf3a2d38f40cd07ab3dca5a3233d01
Temich-Kawaii-Nya <temich-kawaii-nya@mail.ru>
2024-12-31 19:26:23 +0700

    model validation fix

--- app/server/routes/users.py
+++ app/server/routes/users.py
@@ -1,3 +1,4 @@
+from beanie import PydanticObjectId
 from fastapi import APIRouter, HTTPException, status
 
 from app.server.models.user import User
@@ -7,3 +8,6 @@ router = APIRouter()
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user(user: User):
     await user.insert()
+@router.get("/{user_id}", status_code=status.HTTP_200_OK, response_model=User)
+async def read_user(user_id: PydanticObjectId):
+    return await User.get(user_id)
